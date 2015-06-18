2a231c4ac850f91fbac8f9b06fb3341e00526cdd
strollhy <strollhuyac@gmail.com>
2015-06-17 22:36:02 -0500

    Finish lights update logic and implement status output

--- main.py
+++ main.py
@@ -4,19 +4,18 @@ from traffic import *
 from rule import Rule
 
 ######################################################
-fake_data = dict()
-fake_data["cars"] = [[1, "1,2", 0],
-                     [2, "2,3", 1],
-                     [3, "3,2", 10]]
-fake_data["links"] = [[1, 1000, 4, 20, 50, 220, 4, 5, 5],
-                      [2, 800, 4, 20, 50, 220, 4, 5, 5],
-                      [3, 900, 4, 20, 50, 220, 4, 5, 5]]
-fake_data["link_link"] = [[1, 2, 0, 0],
-                          [2, 0, 0, 3],
-                          [3, 0, 0, 0]]
-fake_data["lights"] = [[1, "40,30", "50,50", "30,40"],
-                       [2, "40,30", "50,50", "30,40"],
-                       [3, "40,30", "50,50", "30,40"],]
+car_data = [["1", "1,2", "0"],
+            ["2", "2,3", "1"],
+            ["3", "3,2", "10"]]
+link_data = [["1000", "4", "20", "50", "220", "4", "5", "5"],
+             ["800", "4", "20", "50", "220", "4", "5", "5"],
+             ["900", "4", "20", "50", "220", "4", "5", "5"]]
+link_link_data = [["1", "2", "0", "0"],
+                  ["2", "0", "0", "3"],
+                  ["3", "0", "0", "0"]]
+light_data = [["1", "10,20", "10,20", "10,20"],
+              ["2", "40,30", "50,50", "30,40"],
+              ["3", "40,30", "50,50", "30,40"]]
 
 ######################################################
 
@@ -32,8 +31,8 @@ class Simulator:
         self.rule = Rule(self)
 
     def start(self):
-        # TODO load from files
-        self.next_time_stamp()
+        for i in xrange(50):
+            self.next_time_stamp()
 
     def next_time_stamp(self):
         self.update_lights()
@@ -41,39 +40,44 @@ class Simulator:
         self.update_links()
         self.time += 1
 
+        self.print_status()
+
     def setup_cars(self):
         # Setup cars
-        for data in fake_data["cars"]:
+        for data in car_data:
             self.cars[data[0]] = Car(data)
 
     def setup_links(self):
         # Setup links
-        for data in fake_data["links"]:
-            self.links[data[0]] = Link(data)
+        for i in xrange(len(link_data)):
+            link_id = str(i + 1)
+            self.links[link_id] = Link(link_id, *link_data[i])
 
         # Setup link connections
-        for data in fake_data["link_link"]:
-            link_id = data[0]
-            if data[1]:
+        for i in xrange(len(link_link_data)):
+            data = link_link_data[i]
+            link_id = str(i + 1)
+
+            if data[1] != "0":
                 self.links[link_id].left_link = data[1]
                 self.links[data[1]].right_link = link_id
-            if data[2]:
+            if data[2] != "0":
                 self.links[link_id].right_link = data[2]
                 self.links[data[2]].left_link = link_id
-            if data[3]:
+            if data[3] != "0":
                 self.links[link_id].through_link = data[3]
                 self.links[data[3]].through_link = link_id
 
     def setup_lights(self):
         # Setup nodes
-        for data in fake_data["lights"]:
+        for data in light_data:
             link_id = data[0]
-            for light_data in data[1:]:
-                self.links[link_id].lights.append(Light(*light_data.split(',')))
+            for light in data[1:]:
+                self.links[link_id].lights.append(Light(*light.split(',')))
 
     def update_lights(self):
         for link in self.links.values():
-            link.update_lights()
+            link.update_lights(self.time)
 
     def update_cars(self):
         self.rule.update_cars()
@@ -81,6 +85,11 @@ class Simulator:
     def update_links(self):
         self.rule.update_links()
 
+    # outputs
+    def print_status(self):
+        print "==== Time Stamp %d =====" % self.time
+        for link in self.links.values():
+            link.print_lights()
 
 if __name__ == '__main__':
     simulator = Simulator()
--- traffic.py
+++ traffic.py
@@ -14,8 +14,8 @@ class Car():
 
 class Light():
     def __init__(self, red_period, green_period, light=0):
-        self.red_period = red_period
-        self.green_period = green_period
+        self.red_period = int(red_period)
+        self.green_period = int(green_period)
         self.light = light
 
     def clone(self):
@@ -24,24 +24,31 @@ class Light():
     def reverse_clone(self):
         return Light(self.green_period, self.red_period, 1 - self.light)
 
-    def update_status(self):
-        pass
+    def update_status(self, time):
+        if self.light:
+            if self.green_period <= time:
+                self.light = 0
+        else:
+            if self.red_period <= time:
+                self.light = 1
 
 
 class Link():
-    def __init__(self, *args):
-        self.link_id = ()
-        self.left_link = 0
-        self.right_link = 0
-        self.through_link = 0
-        self.length = 0
-        self.num_of_lanes=0
+    def __init__(self, link_id, length, num_of_lanes, min_speed,
+                 max_speed, jam_density, left_cap, right_cap, through_cap):
+        self.link_id = link_id
+        self.length = length
+        self.num_of_lanes = num_of_lanes
+        self.max_speed = min_speed
+        self.min_speed = max_speed
+        self.jam_density = jam_density
+        self.left_cap = left_cap
+        self.right_cap = right_cap
+        self.through_cap = through_cap
+
         self.num_of_cars = 0
         self.capacity = 0
-        self.max_speed = 0
-        self.min_speed = 0
         self.avg_speed = 0
-        self.avg_density = 0
         self.sublink_1 = [] #Cars in Sublink1
         self.sublink_2 = [] #Cars in Sublink2
         self.left_lane = [] #cars in the last left lane
@@ -49,6 +56,12 @@ class Link():
         self.through_lane = [] #cars in the last through lane
         self.lights = []
 
-    def update_lights(self):
+    def update_lights(self, time):
         for light in self.lights:
-            light.update_status()
+            light.update_status(time)
+
+    def get_lights(self):
+        return ",".join([str(light.light) for light in self.lights])
+
+    def print_lights(self):
+        print "Link #%s::" % self.link_id + self.get_lights()
